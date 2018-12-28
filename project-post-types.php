862b3351da55fcbd102cbc42964cb6ac07bbb829
Thomas Bockhorn <thomasbockhorn1863@gmail.com>
2018-12-28 10:09:59 -0600

    Added an mu-plugin that makes it essential for a project-post tytpe

---
+++ project-post-types.php
@@ -0,0 +1,18 @@
+<?php
+/*This function creates a custom post type of Projects */
+function projects_post_type()
+{
+    register_post_type('project', array(
+        'public' => true,
+        'labels' => array(
+            'name' => 'Projects',
+            'add_new_item' => 'Add New Project',
+            'edit_item' => 'Edit Project',
+            'add_items' => 'All Projects',
+            'singular_name' => 'Project'
+        ),
+        'menu_icon' => 'dashicons-format-aside'
+    ));
+}
+add_action('init', 'projects_post_type');
+?>
